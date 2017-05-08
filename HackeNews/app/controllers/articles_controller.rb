ef019a53157215b084ec7dbb7dcb433807b18daf
Daniel Azevedo <daniel96.azevedo@gmail.com>
2017-05-08 10:12:23 +0100

    update feature added

--- HackeNews/app/controllers/articles_controller.rb
+++ HackeNews/app/controllers/articles_controller.rb
@@ -1,12 +1,27 @@
 class ArticlesController < ApplicationController
 
 	def new
-	
+	  @article = Article.new	
 	end
 
 	def show
     	  @article = Article.find(params[:id])
 	end
+
+	def edit
+	  @article = Article.find(params[:id])
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+ 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
 
 	def create
 	  @article = Article.new(article_params)
