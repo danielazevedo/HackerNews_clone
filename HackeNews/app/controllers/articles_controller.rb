34ab4d633bbe8ec25a42fadcd8b54e904c2ac1b7
Daniel Azevedo <daniel96.azevedo@gmail.com>
2017-05-08 10:29:09 +0100

    destroy feature added

--- HackeNews/app/controllers/articles_controller.rb
+++ HackeNews/app/controllers/articles_controller.rb
@@ -11,6 +11,14 @@ class ArticlesController < ApplicationController
 	def edit
 	  @article = Article.find(params[:id])
 	end
+	
+	
+	def destroy
+	  @article = Article.find(params[:id])
+	  @article.destroy
+	 
+	  redirect_to articles_path
+	end	
 
 	def update
 	  @article = Article.find(params[:id])
